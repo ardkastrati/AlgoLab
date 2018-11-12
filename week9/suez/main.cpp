7284234b85ae14e6843f9a5177bca020e67ee155
Ard Kastrati <kard@stud14-h56.inf.ethz.ch>
2018-11-12 20:29:34 +0100

    Implemented potw9 for all points

--- week9/suez/main.cpp
+++ week9/suez/main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include <cassert>
 #include <algorithm>
+#include <limits>
 #include <CGAL/QP_models.h>
 #include <CGAL/QP_functions.h>
 #include <CGAL/Gmpz.h>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Delaunay_triangulation_2.h>
-
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Delaunay_triangulation_2<K>  Triangulation;
-typedef Triangulation::Edge_iterator  Edge_iterator;
 
 // choose exact integral type
 typedef CGAL::Gmpz ET;
@@ -21,6 +16,8 @@ typedef CGAL::Quadratic_program_solution<ET> Solution;
 
 typedef std::pair<int, int> P;
 typedef std::vector<P> VP;
+
+double MAX = std::numeric_limits<double>::max();
 
 double ceil_to_double(const Q& x) {
         double a = std::ceil(CGAL::to_double(x));
@@ -52,21 +49,33 @@ void testcase(int kali) {
 	//take care of old posters
 	int k = 0;	
 	for(int i = 0; i < n; i++) {
+		//Find the nearest
+		double dist = MAX; int best = -1;
 		for(int j = 0; j < m; j++) {
 			double dist1 = ((double) std::abs(free_nails[i].first - occupied_nails[j].first))/w;
 			double dist2 = ((double) std::abs(free_nails[i].second - occupied_nails[j].second))/h;
+			double new_dist = std::max(dist1, dist2);
+			if(new_dist < dist) {
+				dist = new_dist;
+				best = j;
+			}	
+		}
+		if(best != -1) {
+			//Create now only one constraint
+			double dist1 = ((double) std::abs(free_nails[i].first - occupied_nails[best].first))/w;
+			double dist2 = ((double) std::abs(free_nails[i].second - occupied_nails[best].second))/h;
 			if(dist1 > dist2) {
 				//I should compare x's
-				if(free_nails[i].first <= occupied_nails[j].first) {
-					lp.set_a(i, k, w); lp.set_b(k, 2*(occupied_nails[j].first - free_nails[i].first) - w);
+				if(free_nails[i].first <= occupied_nails[best].first) {
+					lp.set_a(i, k, w); lp.set_b(k, 2*(occupied_nails[best].first - free_nails[i].first) - w);
 				} else {
-					lp.set_a(i, k, w); lp.set_b(k, -2*(occupied_nails[j].first - free_nails[i].first) - w);
+					lp.set_a(i, k, w); lp.set_b(k, -2*(occupied_nails[best].first - free_nails[i].first) - w);
 				}
 			}  else {
-				if(free_nails[i].second <= occupied_nails[j].second) {
-					lp.set_a(i, k, h); lp.set_b(k, 2*(occupied_nails[j].second - free_nails[i].second) - h);
+				if(free_nails[i].second <= occupied_nails[best].second) {
+					lp.set_a(i, k, h); lp.set_b(k, 2*(occupied_nails[best].second - free_nails[i].second) - h);
 				} else {
-					lp.set_a(i, k, h); lp.set_b(k, -2*(occupied_nails[j].second - free_nails[i].second) - h);
+					lp.set_a(i, k, h); lp.set_b(k, -2*(occupied_nails[best].second - free_nails[i].second) - h);
 				}
 			}
 			k++;
